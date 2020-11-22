cb94cef45297502b9c47a0ab52698125fa6cd5ba
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2020-11-21 19:52:36 -0600

    church in urbana html files

--- index.js
+++ index.js
@@ -4,16 +4,41 @@ var cons = require('consolidate');
 const path = require('path');
 
 app.use('/', express.static('public'));
-// app.use('/', express.static('public/css'));
-// app.use('/', express.static('public/js'));
 
 app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
+app.get('/ch-additional-clinks.html' ,(req, res)=>{
+  res.render("ch-additional-clinks.html");
+})
+
+app.get('/ch-campus-ministry.html' ,(req, res)=>{
+  res.render("ch-campus-ministry.html");
+})
+
 app.get('/' ,(req, res)=>{
-  res.render("index.html");
-  console.log('successfully render the index.html')
+  res.render("ch-churchinurbana.html");
+})
+
+app.get('/ch-churchinurbana.html' ,(req, res)=>{
+  res.render("ch-churchinurbana.html");
+})
+
+app.get('/ch-college-training.html' ,(req, res)=>{
+  res.render("ch-college-training.html");
+})
+
+app.get('/ch-conferences-and-trainings.html' ,(req, res)=>{
+  res.render("ch-conferences-and-trainings.html");
+})
+
+app.get('/ch-our-meeting.html' ,(req, res)=>{
+  res.render("ch-our-meeting.html");
+})
+
+app.get('/ch-statement-of-faith.html' ,(req, res)=>{
+  res.render("ch-statement-of-faith.html");
 })
 
 app.listen(process.env.PORT || 3003, function () {
